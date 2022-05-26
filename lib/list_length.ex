774146593b2e933228eb4fadd76198d77afad2da
ICTS <acer@ICTSAM-0828.ICTS.BR>
2022-05-26 09:14:31 -0400

    finish

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,29 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call([]), do: 0
+
+  def call(list) do
+    list_length(list, 0)
   end
+
+  defp list_length([_head | tail], acc) do
+    acc = acc + 1
+    list_length(tail, acc)
+  end
+
+  defp list_length([], acc), do: acc
 end
+
+# defmodule SumList do
+#   def call([]), do: {:error, "List is empty"}
+
+#   def call(list) do
+#     sum_list(list, 0)
+#   end
+
+#   defp sum_list([], acc), do: acc
+
+#   defp sum_list([head | tail], acc) do
+#     acc = acc + head
+#     sum_list(tail, acc)
+#   end
+# end
